fb3e20bbe9a96488465c66bdea77d2826cac9a82
RichardDuclos <89917967+RichardDuclos@users.noreply.github.com>
2025-02-26 17:34:58 +0100

    initial commit Done: Fetch brands

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {BrandFilterComponent} from './components/brands/brands-list/brand-filter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BrandFilterComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {BrowseMotosComponent} from './pages/browse-motos/browse-motos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'browse', component: BrowseMotosComponent},
+  { path: '', redirectTo: "/browse", pathMatch: 'full'},
+
+];
